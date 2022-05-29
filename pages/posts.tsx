0a4eea8bc479df5cc8fbd7e18c43d4a6f92e2e34
Stephan-Neo <stepan.kazancev.04@bk.ru>
2022-05-30 00:03:36 +0600

    add localstorage

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -9,15 +9,29 @@ export default function Posts(){
     const [key, setKey] = useState('23')
 
     useEffect(() => {
-        console.log('done')
-        fetchAphorisms()
-        console.log(aphorisms)
+        if(localStorage.getItem("load") !== 'true'){
+            fetchAphorisms()
+        }else{
+            setAphorisms([{"q":"When the fish is caught we pay no more attention to the trap.","a":"Huang Po","c":"61","h":"<blockquote>&ldquo;When the fish is caught we pay no more attention to the trap.&rdquo; &mdash; <footer>Huang Po</footer></blockquote>"}])
+        }
     }, [])
 
     async function fetchAphorisms(){
         try{
             const response = await axios.get<IPost[]>(`https://zenquotes.io/api/quotes/${key}&custom=true`)
-            setAphorisms(response.data)
+            setAphorisms(response.data.slice(1, 10))
+
+            if(response.status === 200){
+                localStorage.setItem("load", 'true')
+                localStorage.setItem("data", JSON.stringify(response.data.slice(1, 10)))
+                response.data.map((data) => {
+                    for(let i = 0; i < 10; i++){
+                        localStorage.setItem(`id_${i}`, JSON.stringify(data))
+                    }
+                    
+                })
+              
+            } 
         } catch (e){
             
         }
